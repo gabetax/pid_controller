53e692d5896ea4b26855c6b0bd2ca0d55130c49e
Gabe Martin-Dempesy <gabetax@gmail.com>
2017-11-28 18:40:59 -0800

    Add output and integral windup bounds

--- lib/pid_controller.rb
+++ lib/pid_controller.rb
@@ -4,15 +4,42 @@ require 'pid_controller/version'
 class PidController
   attr_accessor :setpoint, :kp, :ki, :kd
 
-  def initialize(setpoint:, kp: 1.0, ki: 1.0, kd: 1.0)
+  def initialize(
+    setpoint:,
+    kp: 1.0,
+    ki: 1.0,
+    kd: 1.0,
+    integral_min: nil,
+    integral_max: nil,
+    output_min: nil,
+    output_max: nil
+  )
     @setpoint = setpoint
     @kp = kp
     @ki = ki
     @kd = kd
+    # Prevents https://en.wikipedia.org/wiki/Integral_windup via bounds
+    @integral_min = integral_min || -Float::INFINITY
+    @integral_max = integral_max || Float::INFINITY
+    @output_min = output_min || -Float::INFINITY
+    @output_max = output_max || Float::INFINITY
     @integral   = 0.0
     @derivative = 0.0
     @last_error  = nil
     @last_update = nil
+  end
+
+  unless defined?(0.clamp)
+    # Comparable#clamp is introduced in Ruby 2.4
+    module NumericClamp
+      # I'd refine Comparable itself, but older Rubies can't refine modules
+      refine Numeric do
+        def clamp(min, max)
+          [min, [max, self].min].max
+        end
+      end
+    end
+    using NumericClamp
   end
 
   def update(measurement)
@@ -32,7 +59,7 @@ class PidController
     error = setpoint - measurement.to_f
 
     if dt > 0.0
-      @integral += error * dt
+      @integral = (@integral + error * dt).clamp(@integral_min, @integral_max)
       @derivative = (error - @last_error) / dt
     end
 
@@ -42,7 +69,7 @@ class PidController
   end
 
   def output
-    p_term + i_term + d_term
+    (p_term + i_term + d_term).clamp(@output_min, @output_max)
   end
 
   def p_term
@@ -57,6 +84,7 @@ class PidController
     kd * @derivative
   end
 
+  # Read the monotonic clock. It avoid horrors of leap seconds and NTP.
   def clock_time
     Process.clock_gettime(Process::CLOCK_MONOTONIC)
   end
